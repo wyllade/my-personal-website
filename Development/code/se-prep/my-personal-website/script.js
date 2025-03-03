c19e9614408bdcb8fc660f5ea56566dd8c791dca
Emman Oduor <wyllademanu003@gmail.com>
2025-03-03 18:50:41 +0300

    initiate commit

---
+++ Development/code/se-prep/my-personal-website/script.js
@@ -0,0 +1,9 @@
+// script.js
+function showMoreArt() {
+    var moreArtSection = document.getElementById("more-art");
+    if (moreArtSection.style.display === "none") {
+        moreArtSection.style.display = "block";
+    } else {
+        moreArtSection.style.display = "none";
+    }
+}
